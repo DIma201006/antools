8ab81a1080a519181e6058059c7cb0875c8c61e9
DIma201006 <166295049+DIma201006@users.noreply.github.com>
2024-08-10 19:10:17 +0300

    gggg

--- script.js
+++ script.js
@@ -1,13 +1,108 @@
-document.querySelectorAll('.button-like').forEach(item => {
-    item.addEventListener('click', function () {
-        if (document.getElementById('like').classList.contains('active')) {
-            document.getElementById('like').classList.remove('active');
-            document.getElementById('red-like').classList.add('active');
+document.getElementById('button-like-1').addEventListener('click', function () {
+        if (document.getElementById('like-1').classList.contains('active')) {
+            document.getElementById('like-1').classList.remove('active');
+            document.getElementById('red-like-1').classList.add('active');
             return
         } else 
-        document.getElementById('red-like').classList.remove('active');
-        document.getElementById('like').classList.add('active');
-       
-     })
-});
+        document.getElementById('red-like-1').classList.remove('active');
+        document.getElementById('like-1').classList.add('active'); 
+     });
     
+document.getElementById('button-like-2').addEventListener('click', function () {
+        if (document.getElementById('like-2').classList.contains('active')) {
+            document.getElementById('like-2').classList.remove('active');
+            document.getElementById('red-like-2').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-2').classList.remove('active');
+        document.getElementById('like-2').classList.add('active'); 
+});
+     
+document.getElementById('button-like-3').addEventListener('click', function () {
+        if (document.getElementById('like-3').classList.contains('active')) {
+            document.getElementById('like-3').classList.remove('active');
+            document.getElementById('red-like-3').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-3').classList.remove('active');
+        document.getElementById('like-3').classList.add('active'); 
+});
+     
+document.getElementById('button-like-4').addEventListener('click', function () {
+        if (document.getElementById('like-4').classList.contains('active')) {
+            document.getElementById('like-4').classList.remove('active');
+            document.getElementById('red-like-4').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-4').classList.remove('active');
+        document.getElementById('like-4').classList.add('active'); 
+});
+     
+
+document.getElementById('button-like-5').addEventListener('click', function () {
+        if (document.getElementById('like-5').classList.contains('active')) {
+            document.getElementById('like-5').classList.remove('active');
+            document.getElementById('red-like-5').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-5').classList.remove('active');
+        document.getElementById('like-5').classList.add('active'); 
+});
+     
+
+document.getElementById('button-like-6').addEventListener('click', function () {
+        if (document.getElementById('like-6').classList.contains('active')) {
+            document.getElementById('like-6').classList.remove('active');
+            document.getElementById('red-like-6').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-6').classList.remove('active');
+        document.getElementById('like-6').classList.add('active'); 
+});
+
+
+     document.getElementById('button-like-7').addEventListener('click', function () {
+        if (document.getElementById('like-7').classList.contains('active')) {
+            document.getElementById('like-7').classList.remove('active');
+            document.getElementById('red-like-7').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-7').classList.remove('active');
+        document.getElementById('like-7').classList.add('active'); 
+     });
+
+
+          document.getElementById('button-like-8').addEventListener('click', function () {
+        if (document.getElementById('like-8').classList.contains('active')) {
+            document.getElementById('like-8').classList.remove('active');
+            document.getElementById('red-like-8').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-8').classList.remove('active');
+        document.getElementById('like-8').classList.add('active'); 
+          });
+     
+
+               document.getElementById('button-like-9').addEventListener('click', function () {
+        if (document.getElementById('like-9').classList.contains('active')) {
+            document.getElementById('like-9').classList.remove('active');
+            document.getElementById('red-like-9').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-9').classList.remove('active');
+        document.getElementById('like-9').classList.add('active'); 
+     });
+
+
+
+          document.getElementById('button-like-10').addEventListener('click', function () {
+        if (document.getElementById('like-10').classList.contains('active')) {
+            document.getElementById('like-10').classList.remove('active');
+            document.getElementById('red-like-10').classList.add('active');
+            return
+        } else 
+        document.getElementById('red-like-10').classList.remove('active');
+        document.getElementById('like-10').classList.add('active'); 
+     });
+
+     
